dd4e9dcdd296151a16133fd20054b51747f79518
i824354 <marcelo.rescigno@sap.com>
2026-05-28 10:36:15 -0300

    Completed shopping project from lecture 4

--- 4_Learning/shopping/shopping.py
+++ 4_Learning/shopping/shopping.py
@@ -8,7 +8,6 @@ TEST_SIZE = 0.4
 
 
 def main():
-
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
@@ -59,7 +58,53 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    month_map = {
+        "Jan": 0,
+        "Feb": 1,
+        "Mar": 2,
+        "Apr": 3,
+        "May": 4,
+        "June": 5,
+        "Jul": 6,
+        "Aug": 7,
+        "Sep": 8,
+        "Oct": 9,
+        "Nov": 10,
+        "Dec": 11,
+    }
+    visitor_type_map = {"Returning_Visitor": 1, "New_Visitor": 0, "Other": 0}
+    bool_map = {"TRUE": 1, "FALSE": 0}
+
+    with open(filename, newline="") as csvfile:
+        filereader = csv.reader(csvfile)
+        next(filereader)  # This will skipp the header line
+        evidence = []
+        labels = []
+        for row in filereader:
+            evidence.append(
+                (
+                    int(row[0]),
+                    float(row[1]),
+                    int(row[2]),
+                    float(row[3]),
+                    int(row[4]),
+                    float(row[5]),
+                    float(row[6]),
+                    float(row[7]),
+                    float(row[8]),
+                    float(row[9]),
+                    month_map[row[10]],  # Month
+                    int(row[11]),
+                    int(row[12]),
+                    int(row[13]),
+                    int(row[14]),
+                    visitor_type_map[row[15]],  # VisitorType
+                    bool_map[row[16]],  # Weekend
+                )
+            )
+            # int, float, int, float, int, float, float, float, float, float, int*Month*, int, int, int, int, int*VisitorType*, int*Weekend*
+            labels.append(bool_map[row[17]])  # TRUE = 1 FALSE = 0
+        return (evidence, labels)
 
 
 def train_model(evidence, labels):
@@ -67,7 +112,9 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    kn_model = KNeighborsClassifier(n_neighbors=1)
+    kn_model.fit(evidence, labels)
+    return kn_model
 
 
 def evaluate(labels, predictions):
@@ -85,8 +132,28 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
 
+    total_labeled_positive = 0
+    total_labeled_negative = 0
+
+    correct_positive = 0
+    correct_negative = 0
+
+    for label, prediction in zip(labels, predictions):
+        if label == 1:
+            total_labeled_positive += 1
+            if prediction  == 1:
+                correct_positive += 1
+        else:
+            total_labeled_negative += 1
+            if prediction == 0:
+                correct_negative += 1
+
+    
+    sensitivity = correct_positive / total_labeled_positive
+    specificity = correct_negative / total_labeled_negative
+
+    return (sensitivity, specificity)
 
 if __name__ == "__main__":
     main()
